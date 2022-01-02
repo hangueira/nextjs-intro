4383b4d1dc04333cd16e51cbf667eee6b9c9f54b
hankyungsu <hks713@naver.com>
2022-01-02 22:00:30 +0900

    git test

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>Hello {counter}</h1>
+      <h1>Hello 경수 {counter}</h1>
       <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </div>
   );
